c5d6e928732e2db4c5c9278c83fd5b91b6c41dbe
shakilmahammud <hshakil987@gmail>
2020-12-17 00:31:16 +0600

    next done

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+/*Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+// export default (req, res) => {
+//   res.statusCode = 200
+//   res.json({ name: 'John Doe' })
+// } */
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function (req:NextApiRequest, res:NextApiResponse) {
+ // it Shoulfd Not Be a React Component
+
+ res.json({status: 'OK'})
+
 }
